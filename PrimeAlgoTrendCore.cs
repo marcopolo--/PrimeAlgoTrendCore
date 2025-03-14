57c01c013c6eea07c8c8a5426606762c31898249
amch <amch@bluewin.ch>
2025-03-14 18:04:18 +0100

    weekly update

--- PrimeAlgoTrendCore.cs
+++ PrimeAlgoTrendCore.cs
@@ -9,19 +9,27 @@
 
 using cAlgo.API;
 using cAlgo.API.Indicators;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+
 
 namespace cAlgo.Robots
 {
-    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None, AddIndicators = true)]
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.FullAccess, AddIndicators = true)]
     public class PrimeAlgoTrendCore : Robot
     {
         private double _volumeInUnits;
 
-        private PrimeAlgoKFMRcore fastPrimeAlgoKFMRcore;
+        private primeAlgoKalmanMeanReversion longTermprimeAlgoKalmanMeanReversion;
+        private PrimeAlgoKFMRcore ltPrimeAlgoKFMRcore;
 
         private primeAlgoKalmanMeanReversion slowprimeAlgoKalmanMeanReversion;
         
-        private primeAlgoKalmanMeanReversion longTermprimeAlgoKalmanMeanReversion;
+        
 
         [Parameter("Source", Group = "Fast MA")]
         public DataSeries FastMaSource { get; set; }
@@ -58,34 +66,91 @@ namespace cAlgo.Robots
         protected override void OnStart()
         {
             _volumeInUnits = Symbol.QuantityToVolumeInUnits(VolumeInLots);
+            
+            longTermprimeAlgoKalmanMeanReversion = Indicators.GetIndicator<primeAlgoKalmanMeanReversion>(MarketData.GetBars(TimeFrame.Renko20).ClosePrices, true, 30);
+            ltPrimeAlgoKFMRcore = Indicators.GetIndicator<PrimeAlgoKFMRcore>(MarketData.GetBars(TimeFrame.Renko20).ClosePrices, false, 10);
+            
 
-            fastPrimeAlgoKFMRcore = Indicators.GetIndicator<PrimeAlgoKFMRcore>(MarketData.GetBars(TimeFrame.Renko5).ClosePrices, true, FastMaPeriod);
+            //fastPrimeAlgoKFMRcore = Indicators.GetIndicator<PrimeAlgoKFMRcore>(MarketData.GetBars(TimeFrame.Renko5).ClosePrices, true, FastMaPeriod);
             slowprimeAlgoKalmanMeanReversion = Indicators.GetIndicator<primeAlgoKalmanMeanReversion>(MarketData.GetBars(TimeFrame.Renko5).ClosePrices, true, SlowMaPeriod);
             
-            longTermprimeAlgoKalmanMeanReversion = Indicators.GetIndicator<primeAlgoKalmanMeanReversion>(MarketData.GetBars(TimeFrame.Renko20).ClosePrices, true, 20);
-
-            fastPrimeAlgoKFMRcore.FilterResult.Line.Color = Color.Blue;
-            slowprimeAlgoKalmanMeanReversion.FilterResult.Line.Color = Color.Red;
+            
+            //fastPrimeAlgoKFMRcore.FilterResult.Line.Color = Color.Blue;
+            slowprimeAlgoKalmanMeanReversion.FilterResult.Line.Color = Color.Blue;
         }
 
-        protected override void OnBar()
+        protected override void OnBarClosed()
         {
-            Print("Fast 'IsRising / IsFalling': " + fastPrimeAlgoKFMRcore.FilterResult.IsRising() + "/" + fastPrimeAlgoKFMRcore.FilterResult.IsFalling());
-            Print("Slow 'IsRising / IsFalling': " + slowprimeAlgoKalmanMeanReversion.FilterResult.IsRising() + "/" + slowprimeAlgoKalmanMeanReversion.FilterResult.IsFalling());
-            Print("Long Term Trend up: " + longTermprimeAlgoKalmanMeanReversion.FilterResultUp[1]);
-            Print("Long Term Trend down: " + longTermprimeAlgoKalmanMeanReversion.FilterResultDown[1]);
-            
-            if (fastPrimeAlgoKFMRcore.FilterResult.IsRising() && slowprimeAlgoKalmanMeanReversion.FilterResult.IsRising() && !double.IsNaN(longTermprimeAlgoKalmanMeanReversion.FilterResultUp[1]))
-            {
-                ClosePositions(TradeType.Sell);
+            Print("Pushover Message");
 
-                ExecuteMarketOrder(TradeType.Buy, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+            var parameters = new Dictionary<string, string>
+            {
+                { "token", "5ZpwCSG3Mz3snPfBVy2WGAcDifezkG" },
+                { "user", "9GMhPCM3oevjc1KdxpKM3YtgSoK7fj" },
+                { "message" , GetMessage() }
+            };
+
+
+            //new FormUrlEncodedContent(parameters);
+            //using var client = new HttpClient();
+            //var response = client.PostAsJsonAsync(("https://api.pushover.net/1/messages.json", new FormUrlEncodedContent(parameters)));
+            
+            using (HttpClient client = new HttpClient())
+            {
+                var webRequest = new HttpRequestMessage(System.Net.Http.HttpMethod.Post, "https://api.pushover.net/1/messages.json")
+                {
+                    Content = new StringContent(JsonSerializer.Serialize(parameters), Encoding.UTF8, "application/json")
+                };
+
+                var response = client.Send(webRequest);
+                StreamReader reader = new StreamReader(response.Content.ReadAsStream());
+                Print(reader.ToString());
             }
-            else if (fastPrimeAlgoKFMRcore.FilterResult.IsFalling() && slowprimeAlgoKalmanMeanReversion.FilterResult.IsFalling() && !double.IsNaN(longTermprimeAlgoKalmanMeanReversion.FilterResultDown[1]))
+
+            
+            if (//fastPrimeAlgoKFMRcore.FilterResult.IsFalling() && 
+                slowprimeAlgoKalmanMeanReversion.FilterResult.IsFalling() &&
+                double.IsNaN(longTermprimeAlgoKalmanMeanReversion.FilterResultUp.LastValue) &&
+                double.IsNaN(longTermprimeAlgoKalmanMeanReversion.FilterResultDown.LastValue)
+                )
             {
                 ClosePositions(TradeType.Buy);
+            }
+            
+            if (//fastPrimeAlgoKFMRcore.FilterResult.IsRising() &&
+                slowprimeAlgoKalmanMeanReversion.FilterResult.IsRising() &&
+                double.IsNaN(longTermprimeAlgoKalmanMeanReversion.FilterResultUp.LastValue) &&
+                double.IsNaN(longTermprimeAlgoKalmanMeanReversion.FilterResultDown.LastValue)
+                )
+            {
+                ClosePositions(TradeType.Sell);
+            }
+            
+            if (//fastPrimeAlgoKFMRcore.FilterResult.IsRising() &&
+                slowprimeAlgoKalmanMeanReversion.FilterResult.IsRising() &&
+                !double.IsNaN(longTermprimeAlgoKalmanMeanReversion.FilterResultUp.LastValue)
+                )
+            {
+                ClosePositions(TradeType.Sell);
+                
+                if (BotPositions.Length == 0)
+                {
+                    ExecuteMarketOrder(TradeType.Buy, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+                }
 
-                ExecuteMarketOrder(TradeType.Sell, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+                
+            }
+            else if (//fastPrimeAlgoKFMRcore.FilterResult.IsFalling() &&
+                    slowprimeAlgoKalmanMeanReversion.FilterResult.IsFalling() && 
+                    !double.IsNaN(longTermprimeAlgoKalmanMeanReversion.FilterResultDown.LastValue)
+                    )
+            {
+                ClosePositions(TradeType.Buy);
+                
+                if (BotPositions.Length == 0)
+                {
+                    ExecuteMarketOrder(TradeType.Sell, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+                }
             }
         }
         
@@ -97,7 +162,69 @@ namespace cAlgo.Robots
                 if (position.TradeType != tradeType) continue;
 
                 ClosePosition(position);
+                
             }
+        }
+        
+        private string GetMessage()
+        {
+            string res = $"{Symbol.Name}   {DateTime.Now:HH:mm}   @{(Bid + Ask) / 2:0.0000(0)}\n";
+            
+            string prev = "none";
+            string curr = "none";
+            
+            //if(!double.IsNaN(longTermprimeAlgoKalmanMeanReversion.FilterResultDown.Last(1)))
+            //    prev = "down";
+            //else if (!double.IsNaN(longTermprimeAlgoKalmanMeanReversion.FilterResultUp.Last(1)))
+            //    prev = "up";
+                
+            //if(!double.IsNaN(longTermprimeAlgoKalmanMeanReversion.FilterResultDown.Last(0)))
+            //    curr = "down";
+            //else if (!double.IsNaN(longTermprimeAlgoKalmanMeanReversion.FilterResultUp.Last(0)))
+            //    curr = "up"; 
+             
+            res = res + $"LT: {GetLongTermDirection(1)} --> {GetLongTermDirection(0)}\n";
+            
+            
+            prev = "none";
+            curr = "none";
+            
+            if(!double.IsNaN(slowprimeAlgoKalmanMeanReversion.FilterResultDown.Last(1)))
+                prev = "down";
+            else if (!double.IsNaN(slowprimeAlgoKalmanMeanReversion.FilterResultUp.Last(1)))
+                prev = "up";
+                
+            if(!double.IsNaN(slowprimeAlgoKalmanMeanReversion.FilterResultDown.Last(0)))
+                curr = "down";
+            else if (!double.IsNaN(slowprimeAlgoKalmanMeanReversion.FilterResultUp.Last(0)))
+                curr = "up"; 
+             
+            res = res + $"ST: {prev} --> {curr}\n";
+            
+            
+            return res;
+        }
+        
+        private string GetLongTermDirection(int index)
+        {
+            string res = "none";
+            
+            if (!double.IsNaN(longTermprimeAlgoKalmanMeanReversion.FilterResultUp.Last(index)))
+            {
+                res = "up(1)";
+            } else if (!double.IsNaN(longTermprimeAlgoKalmanMeanReversion.FilterResultDown.Last(index)))
+            {
+                res = "down(1)";
+            } else if (!double.IsNaN(ltPrimeAlgoKFMRcore.FilterResultUp.Last(index)))
+            {
+                res = "up(2)";
+            } else if (!double.IsNaN(ltPrimeAlgoKFMRcore.FilterResultDown.Last(index)))
+            {
+                res = "down(2)";
+            }
+            
+            return res;
+        
         }
     }
 }
